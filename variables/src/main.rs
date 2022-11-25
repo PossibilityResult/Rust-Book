2e6fbf6ba99526953de41c36c96fdd631e39220a
Elijah <56851203+eljhfx@users.noreply.github.com>
2022-11-25 12:49:25 -0500

    finished with variables and mutability

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -7,5 +7,19 @@ fn main() {
         println!("The value of x in the inner scope is: {x}");
     }
     println!("The value of x is: {x}");
+
+    let spaces = "   ";
+    println!("spaces: {spaces}");
+
+    let spaces = spaces.len();
+    println!("spaces: {spaces}");
+    
+
+    let mut spaces_two = "   ";
+    println!("spaces_two: {spaces_two}");
+
+    //spaces_two = spaces.len();
+    //println!("spaces_two: {spaces_two}");
+
 }
 
